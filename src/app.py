9f5a2ad8e303de85d90c66b6f8e0cc4433417067
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-07-07 17:53:10 -0500

    Se agrego modulo Movie y se agrego la ruta /api/movies

--- src/app.py
+++ src/app.py
@@ -1,6 +1,8 @@
 from flask import Flask
-
 from config import config # Se importa diccionario del archivo config
+
+# Rutas
+from routes import Movie # se importa el archivo movie y para que lo reconosca como un paquete de crea el archivo __init__.py en la carpeta donde esta el archivo Movie.py
 
 app = Flask(__name__)
 
@@ -9,6 +11,10 @@ def page_not_fount(error):  # Para las paginas no encontradas mostrara este text
 
 if __name__ == '__main__':
     app.config.from_object(config['development'])
+
+# Blueprints   para asignar las  rutas
+    app.register_blueprint(Movie.main, url_prefix='/api/movies') # cuando escriba esta direccion lo que va hacer es mandarme a la direccion raiz
+
 
     # Manejadores de error
     app.register_error_handler(404, page_not_fount) # le decimos al manejador de error que cuando sea el codigo 404  utilice la funcion page_not_fount
