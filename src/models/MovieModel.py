9322027dc2bb0eb3d7a7d3d440d95a17d9857447
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-07-12 17:17:23 -0500

    Se ingreso el for para iteraciones

---
+++ src/models/MovieModel.py
@@ -0,0 +1,23 @@
+from database.db import get_connection
+from .entities.Movie import Movie
+
+class MovieModel():
+    
+    @classmethod
+    def get_movies(self):
+        try:
+            connection=get_connection() # Para que me obtenga la conección
+            movies = []
+            
+            with connection.cursor() as cursor: # Esta es la conección a la base de datos
+                cursor.execute("SELECT id, title, duration, released FROM movie ORDER BY title ASC") # se hace la consulta directamente de forma acendente
+                resultset = cursor.fetchall() 
+                
+                for row in resultset:
+                    movie = Movie(row[0], row[1], row[2], row[3]) #Es para que me muestre los 4 datos cada iteracion del "for"
+                    movies.append(movie.to_JSON()) # La conneción la regresa en formato .json
+                    
+            connection.close() #para cerrar la coneccion
+        except Exception as ex:
+            raise Exception(ex)
+        
