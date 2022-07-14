9864f50fea70cbfd69677f7a5ffce220d6a98c77
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-07-14 16:00:59 -0500

    Correcion faltaba una s  en 'PGSQL

--- src/database/db.py
+++ src/database/db.py
@@ -6,10 +6,10 @@ from decouple import config  # para obtener las variables de entornocono el usua
 def get_connection(): # esta función es para hacer la conección con el servidor
     try:
         return psycopg2.connect(
-            host = config(' PGSQL_HOST'),
-            user = config('PGQL_USER'),
-            password = config('PGQL_PASSWORD'),
-            database = config('PGQL_DATABASE')
+            host = config('PGSQL_HOST'),
+            user = config('PGSQL_USER'),
+            password = config('PGSQL_PASSWORD'),
+            database = config('PGSQL_DATABASE')
         )
     except DatabaseError as ex: # esto es por si falla levanta un error DatabaseError
         raise ex
