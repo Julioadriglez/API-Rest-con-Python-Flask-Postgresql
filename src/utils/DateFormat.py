c15726771d1cedb4247c503ef882a1bb7872fcfc
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-07-14 16:05:20 -0500

    Se agrego @classmethod

--- src/utils/DateFormat.py
+++ src/utils/DateFormat.py
@@ -1,6 +1,6 @@
 import datetime    # Se importa para la configuración del formato de la fecha
 
 class DateFormat():
-    
-    def convert_date(self, date):
-        return datetime.datetime.strftime(date, '%d/%m/%Y')
+    @classmethod
+    def convert_date(self,date):
+        return datetime.datetime.strftime(date, '%d/%m/%Y') # el formato a usar esta en este codigo el cual es dia, mes y años
