700c35833e2fd1767edb5dd2fa4a8a9ed426d327
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-07-14 16:04:27 -0500

    Se separaron los modulos

--- src/routes/Movie.py
+++ src/routes/Movie.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, jsonify
-
+from flask import Blueprint
+from flask import jsonify
 # Models
 
 from models.MovieModel import MovieModel
@@ -11,7 +11,9 @@ main = Blueprint('movie_blueprint', __name__)
 @main.route('/')  # Ruta raiz
 def get_movies():
     try:
-        movies = MovieModel.get_movies() # se le manda la los resultados de la iteracion del for 
-        return jsonify(movies) # convierte el archivo en formato .json
+        # se le manda la los resultados de la iteracion del for
+        movies = MovieModel.get_movies()
+        return jsonify(movies)  # convierte el archivo en formato .json
     except Exception as ex:
-        return jsonify({'message': str(ex)}), 500  # Regresa error cuando hay un error por parte del servidor 
+        # Regresa error cuando hay un error por parte del servidor
+        return jsonify({'message': str(ex)}), 500
